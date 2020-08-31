8d6c0798eb046abfd2cd6216c1a65f79fd1ee376
Nahid <nahid.mansuri@gmail.com>
2020-09-01 02:30:47 +0530

    js loader css added

--- script.js
+++ script.js
@@ -3,9 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+
+//Show Loading
+function startLoading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Hide Loading
+function stopLoading() {
+    if(!loader.hidden){
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 
 // Get Quote from API
 async function getQuote(){
+    startLoading();
     const proxyUrl = 'https://corsnewhereproxy.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try{
@@ -25,9 +42,10 @@ async function getQuote(){
             quoteText.classList.remove('long-quote');
         }
         quoteText.innerText = data.quoteText;
-
+        stopLoading();
     } catch(error) {
         getQuote();
+        stopLoading();
     }
 }
 
